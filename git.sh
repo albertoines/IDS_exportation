b746493b717f83f12e729290cfb2c011a1042318
root <root@misp-s5-IP.192.168.153.4>
2023-07-31 20:31:22 +0200

    Rules update: 31/07/2023

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 # Configura las variables para el nombre de usuario, contraseña y ruta del repositorio
 usuario_github="albertoines"
 token_acceso_personal="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"
-repositorio="https://github.com/albertoines/IDS_exportation.git"
+repositorio="https://${usuario_github}:${token_acceso_personal}@github.com/${usuario_github}/IDS_exportation.git"
 DATE=`date +"%d/%m/%Y"`
 
 # Agrega los cambios a Git
@@ -14,8 +14,9 @@ git commit -m "Rules update: $DATE"
 
 # Autenticación usando el token de acceso personal
 
-git config --global credential.helper "store --file ~/.git-credentials"
-echo -e "protocol=https\nhost=github.com\nusername=$usuario_github\ntoken=$token_acceso_personal" > ~/.git-credentials
+git remote remove origin
+git remote add origin "$repositorio"
 
 # Sube los cambios al repositorio remoto
 git push origin main
+
