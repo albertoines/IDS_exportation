6dca4e2e5cd71f4f4deaf38a8e9e95b03f6312ed
albertoines <795801@unizar.es>
2023-08-01 16:29:58 +0200

    Rules update: 01/08/2023

--- git.sh
+++ git.sh
@@ -6,16 +6,13 @@ token_acceso_personal="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"
 repositorio="https://${usuario_github}:${token_acceso_personal}@github.com/${usuario_github}/IDS_exportation.git"
 DATE=`date +"%d/%m/%Y"`
 
+cd /root/IDS_exportation
 
 # Agrega los cambios a Git
 git add .
 
 # Realiza un commit con un mensaje
 git commit -m "Rules update: $DATE"
-
-# Autenticación usando el token de acceso personal
-git remote remove origin
-git remote add origin "$repositorio"
 
 # Sube los cambios al repositorio remoto
 git push origin main
