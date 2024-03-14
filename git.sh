645a81c59556c1ac18780170f12d56b0348c42a5
Alberto <138675329+albertoines@users.noreply.github.com>
2024-03-14 10:43:53 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# Used to update file changes from the local repository to the remote repository
 # Configura las variables para el nombre de usuario, contraseña y ruta del repositorio
 usuario_github="albertoines"
-token_acceso_personal="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"
+token_acceso_personal=""
 repositorio="https://${usuario_github}:${token_acceso_personal}@github.com/${usuario_github}/IDS_exportation.git"
 DATE=`date +"%d/%m/%Y"`
 
